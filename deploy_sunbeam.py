15c2ad35ffb9c5bc60aba5fc74bb7a56b554a1d5
Andre Ruiz <andre.ruiz@gmail.com>
2024-02-04 19:06:50 -0300

    Fixed config user variable

--- deploy_sunbeam.py
+++ deploy_sunbeam.py
@@ -59,7 +59,8 @@ primary_node = nodes.pop(0)
 
 ### Primary node / bootstrap
 
-p_user = primary_node["user"]
+user = config["user"]
+
 p_host_int = primary_node["host-int"]
 p_host_ext = primary_node["host-ext"]
 
@@ -68,23 +69,23 @@ print("DEBUG: installing primary node {} / {}".format(p_host_int, p_host_ext))
 ssh_clean(p_host_ext)
 
 cmd = "sudo snap install openstack --channel {}".format(config['channel'])
-ssh(p_user, p_host_ext, cmd)
+ssh(user, p_host_ext, cmd)
 
 cmd = "sunbeam prepare-node-script | grep -v newgrp | bash -x"
-ssh(p_user, p_host_ext, cmd)
+ssh(user, p_host_ext, cmd)
 
-put(p_user, p_host_ext, "~/preseed.yaml", yaml.dump(config["preseed"]))
+put(user, p_host_ext, "~/preseed.yaml", yaml.dump(config["preseed"]))
 
 cmd = "time sunbeam cluster bootstrap -p ~/preseed.yaml"
 for role in primary_node["roles"]: cmd += f" --role {role}"
-ssh(p_user, p_host_ext, cmd)
+ssh(user, p_host_ext, cmd)
 
-ssh(p_user, p_host_ext, "sunbeam cluster list")
+ssh(user, p_host_ext, "sunbeam cluster list")
 
 ### Other nodes
 
+
 for node in nodes:
-    s_user = node["user"]
     s_host_int = node["host-int"]
     s_host_ext = node["host-ext"]
 
@@ -94,30 +95,30 @@ for node in nodes:
 
     cmd = "sudo snap install openstack --channel {}\n".format(config['channel'])
     cmd += "sunbeam prepare-node-script | grep -v newgrp | bash -x"
-    ssh(s_user, s_host_ext, cmd)
+    ssh(user, s_host_ext, cmd)
 
-    put(s_user, s_host_ext, "~/preseed.yaml", yaml.dump(config["preseed"]))
+    put(user, s_host_ext, "~/preseed.yaml", yaml.dump(config["preseed"]))
 
     cmd = f"sunbeam cluster add --name {s_host_int}"
-    token = token_extract(ssh_capture(p_user, p_host_ext, cmd))
+    token = token_extract(ssh_capture(user, p_host_ext, cmd))
 
     cmd = "time sunbeam cluster join -p ~/preseed.yaml"
     for role in primary_node["roles"]: cmd += f" --role {role}"
     cmd += f" --token {token}"
-    ssh(s_user, s_host_ext, cmd)
+    ssh(user, s_host_ext, cmd)
 
     # get some status
-    ssh(p_user, p_host_ext, "sunbeam cluster list")
+    ssh(user, p_host_ext, "sunbeam cluster list")
 
 if control_count >= 3:
     cmd = "time sunbeam cluster resize"
-    ssh(p_user, p_host_ext, cmd)
+    ssh(user, p_host_ext, cmd)
 
 cmd = "time sunbeam configure -p ~/preseed.yaml --openrc ~/demo-openrc; echo > ~/demo-openrc"
-ssh(p_user, p_host_ext, cmd)
+ssh(user, p_host_ext, cmd)
 
 cmd = "sunbeam openrc > ~/admin-openrc"
-ssh(p_user, p_host_ext, cmd)
+ssh(user, p_host_ext, cmd)
 
 cmd = "sunbeam launch ubuntu --name test"
-ssh(p_user, p_host_ext, cmd)
+ssh(user, p_host_ext, cmd)
