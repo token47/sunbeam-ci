53a86171636114e0d19b70abb6e622b4a8f2e3a5
Andre Ruiz <andre.ruiz@gmail.com>
2024-02-06 18:23:47 -0300

    Added more patterns to spinning icon RE

--- utils.py
+++ utils.py
@@ -56,7 +56,7 @@ def ssh(user, host, cmd):
 def ssh_filtered(user, host, cmd):
     """Same as ssh() but tries to suppress repeated lines in output"""
     stripgarbage1 = re.compile(r"\x1b\[\??[0-9;]*[hlmAGKHF]|\r|\n| *$")
-    stripgarbage2 = re.compile("[⠋⠙⠹⠸⠼] ")
+    stripgarbage2 = re.compile("[⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏] *")
     cmd = f"ssh -o StrictHostKeyChecking=no -tt {user}@{host} 'set -x; {cmd}'"
     debug(f"SSH-FILTERED: {user}@{host}")
     result = subprocess.Popen(cmd, shell=True, encoding="utf-8",
