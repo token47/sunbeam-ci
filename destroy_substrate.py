c615ab14fd6c282eb52914454b5ec6f54206fe34
Andre Ruiz <andre.ruiz@gmail.com>
2024-02-07 15:51:42 -0300

    Also moved maas_url to env var in destroy script

--- destroy_substrate.py
+++ destroy_substrate.py
@@ -15,13 +15,14 @@ def substrate_ob76(input_config): # pylint: disable=redefined-outer-name
     """Implements the ob76 substrate destroy"""
     hosts_qty = len(input_config["roles"])
     rc = exec_cmd("terraform -chdir=terraform/maas destroy -auto-approve -no-color" \
-                  f" -var='maas_hosts_qty={hosts_qty}'" \
-                  " -var='maas_api_url=http://ob76-node0.maas:5240/MAAS'")
+                  f" -var='maas_hosts_qty={hosts_qty}'")
     if rc > 0:
         die("could not run terraform destroy")
 
 
 os.environ['TF_VAR_maas_api_key'] = os.environ.get("JENKINS_API_KEY")
+os.environ["TF_VAR_maas_api_url"] = "http://ob76-node0.maas:5240/MAAS"
+
 input_config_json = os.environ.get("JENKINS_JSON_CONFIG")
 input_config = json.loads(input_config_json)
 if input_config["substrate"] == "ob76":
